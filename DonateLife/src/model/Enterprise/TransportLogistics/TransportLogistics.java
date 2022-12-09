8fb97abad4ee40744e90e2444765c2b314285563
smitiragrawal <agrawal.sm@northeastern.edu>
2022-12-09 15:25:50 -0500

    added transportation logistics

---
+++ DonateLife/src/model/Enterprise/TransportLogistics/TransportLogistics.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.Enterprise.TransportLogistics;
+
+import model.Enterprise.Enterprise;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author harshaljaiswal
+ */
+public class TransportLogistics extends Enterprise {
+    
+    private ArrayList<Handler> handlerdirectory;
+    private ArrayList<Vehicle> vehicledirectory;
+
+    public TransportLogistics(String enterpriseName, String registeryNumber, String address, String username, String password) {
+        super(enterpriseName, registeryNumber, address, username, password);
+    }
+
+    public ArrayList<Handler> getHandlerdirectory() {
+        return handlerdirectory;
+    }
+
+    public void setHandlerdirectory(ArrayList<Handler> handlerdirectory) {
+        this.handlerdirectory = handlerdirectory;
+    }
+
+    public ArrayList<Vehicle> getVehicledirectory() {
+        return vehicledirectory;
+    }
+
+    public void setVehicledirectory(ArrayList<Vehicle> vehicledirectory) {
+        this.vehicledirectory = vehicledirectory;
+    }
+    
+    
+    
+}
